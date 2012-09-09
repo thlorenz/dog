b410c118a1880b66fa44c5dff51b686a87df80b4
Thorsten Lorenz <thlorenz@gmx.de>
2012-09-09 11:25:32 -0400

    renderer tests are now taps

--- test/renderer.js
+++ test/renderer.js
@@ -1,44 +1,41 @@
 /*jshint asi:true, esnext:true */
 
 var dog = require('..')
+  , test = require('tap').test
   , path = require('path')
   , postunoDir = path.join(__dirname, 'fixtures', 'testblog', 'postuno')
   ;
 
-describe('when rendering postuno that has an inlined and an external snippet', function () {
-  var result;
+test('when rendering postuno that has an inlined and an external snippet', function (t) {
 
-  before(function (done) {
-    dog.renderer.render(postunoDir, function (err, html) {
-      if (err) console.error(err); 
-      else result = html;
-      done();
-    });
+  t.plan(4)
+
+  dog.renderer.render(postunoDir, function (err, html) {
+    t.test('# renders main header', function (t) {
+      t.ok(~html.indexOf('<h1>Blog Uno</h1>'))
+      t.end()
+    })
+
+    t.test('# renders inlined snippet', function (t) {
+      t.ok(~html.indexOf('<h2>Inlined snippet</h2>'), 'header')
+      t.ok(~html.indexOf('<code class="keyword">var</code> <code class="plain">some = </code><code class="string">\'javascript\'</code>'), 'snippet')
+      t.end()
+    })
+
+    t.test('# renders external snippet', function (t) {
+      t.ok(~html.indexOf('<h2>Snippet pulled in</h2>'), 'header')
+      t.ok(~html.indexOf('<code class="keyword">var</code> <code class="plain">simple = 1;</code>'), 'snippet')
+      t.end()
+    })
+
+    t.test('# wraps it in blog-post article', function (t) {
+      t.ok(~html.indexOf('<article class="blog-post"'))
+      t.end()
+    })
+
+    t.end()
   })
 
-  it('contains main header', function () {
-    result.should.include('<h1>Blog Uno</h1>');
-  })
-
-  it('contains header for inlined snippet', function () {
-    result.should.include('<h2>Inlined snippet</h2>');
-  })
-
-  it('contains inlined snippet', function () {
-    result.should.include('<code class="keyword">var</code> <code class="plain">some = </code><code class="string">\'javascript\'</code>');
-  })
-
-  it('contains header for external snippet', function () {
-    result.should.include('<h2>Snippet pulled in</h2>');
-  })
-
-  it('contains external snippet', function () {
-    result.should.include('<code class="keyword">var</code> <code class="plain">simple = 1;</code>');
-  })
-
-  it('wraps it in blog-post article', function () {
-    result.should.include('<article class="blog-post"');  
-  })
 })
 
 
